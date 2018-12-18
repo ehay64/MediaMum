bfbb988453297b94ec0f3a12490dc4e895d7e3b3
Eric Hay <erichay@live.ca>
2018-12-18 13:04:25 -0800

    Add basic framework for remote

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Media Mum</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
+        <link rel="stylesheet" href="https://code.getmdl.io/1.2.1/material.red-lime.min.css" />
+        <script defer src="https://code.getmdl.io/1.2.1/material.min.js"></script>
+
+        <script
+            type="text/javascript">
+        </script>
+    </head>
+
+    <body>
+        Hello
+    </body>
+</html>
