7878a3153e936578656fa216900585d3f7cff432
Eric Hay <erichay@live.ca>
2018-12-24 14:59:46 -0800

    Clear background when playing media

--- control/play.php
+++ control/play.php
@@ -1,6 +1,6 @@
 <?php
     $film = $_GET["film"];
-    $command = "killall omxplayer.bin >/dev/null 2>&1; nohup omxplayer --no-keys '../media/$film/film.mp4' >/dev/null 2>&1 &";
+    $command = "killall omxplayer.bin >/dev/null 2>&1; nohup omxplayer -b --no-keys '../media/$film/film.mp4' >/dev/null 2>&1 &";
 
     shell_exec($command);
 ?>
