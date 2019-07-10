8c5beaa367c6b0bea1cafd1984b748639b1d592a
Eric Hay <erichay@live.ca>
2019-07-09 19:12:44 -0700

    Fix play.php to work with symbolic links

--- control/play.php
+++ control/play.php
@@ -1,6 +1,7 @@
 <?php
     $film = $_GET["film"];
-    $command = "killall omxplayer.bin >/dev/null 2>&1; nohup omxplayer -b --no-keys '../media/$film/film.mp4' >/dev/null 2>&1 &";
+    $command = "killall omxplayer.bin >/dev/null 2>/dev/null; nohup omxplayer -b --no-keys '../media/$film/film.mp4' >/dev/null 2>/dev/null &";
 
-    shell_exec($command);
+    $output = shell_exec($command);
+    echo $output;
 ?>
